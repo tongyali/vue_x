9ed0f4039bc13517cd1f6043f6c8fa45a5ee5162
童雅莉 <1046840300@qq.com>
2020-12-07 10:25:53 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <my-add></my-add>
+    <p>-------------华丽的分割线---------------</p>
+    <my-sub></my-sub>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+//导入组件
+import Add from './components/Add'
+import Sub from './components/Sub'
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    'my-add': Add,
+    'my-sub': Sub
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
+<style></style>
---
+++ src/components/Add.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>当前最新的count值为：</p>
+    <button>+1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped></style>
